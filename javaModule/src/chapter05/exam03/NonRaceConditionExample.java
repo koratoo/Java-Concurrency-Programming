d4777aa793a480534f4b2414574bca8debc14cbf
koratoo <ssyk2@naver.com>
2024-02-07 19:57:49 +0900

    NonRaceCondition, RaceCondition

--- javaModule/src/chapter05/exam03/NonRaceConditionExample.java
+++ javaModule/src/chapter05/exam03/NonRaceConditionExample.java
@@ -10,7 +10,21 @@ public class NonRaceConditionExample {
         for (int i = 0; i < incrementThreads.length ; i++) {
             incrementThreads[i] = new Thread(() -> {
 
+                for (int j = 0; j < 10000; j++) {
+                    synchronized (NonRaceConditionExample.class) {
+                        sharedResource++; // 각 스레드가 공유 데이터에 동시에 접근할 수 없다.
+                    }
+                }
             });
+            incrementThreads[i].start();
         }
+
+        // 모든 스레드가 작업을 완료할 때까지 기다림
+        for (int i = 0; i < incrementThreads.length; i++) {
+            incrementThreads[i].join();
+        }
+
+        System.out.println("Expected Value = " + (100 * 10000));
+        System.out.println("Actual Value = " + sharedResource); //순차적으로 계산되기 때문에 매번 실행 때 마다 결과 값이 동일하다.
     }
 }
